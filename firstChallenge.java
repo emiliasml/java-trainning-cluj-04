dd35b41733937fbe7fd01f25604be28248d0c094
emiliasml <62222507+emiliasml@users.noreply.github.com>
2020-04-27 16:03:44 +0300

    Update firstChallenge.java

--- firstChallenge.java
+++ firstChallenge.java
@@ -1,6 +1,6 @@
 package myFirstClass;
 
-public class firstChallenge {
+public class FirstChallenge {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (multipleRizz(i)) System.out.print("Rizz, ");
